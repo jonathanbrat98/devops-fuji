60dc4dda7cf537e0dff88b9397fec7c1465b14b7
jonathanbrat98 <86077588+jonathanbrat98@users.noreply.github.com>
2021-12-07 15:53:02 -0700

    Update App.java

--- src/main/java/com/pingidentity/App.java
+++ src/main/java/com/pingidentity/App.java
@@ -1,6 +1,11 @@
 package com.pingidentity;
 
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Your assignment is to write a method which returns a list of JIRA
@@ -12,12 +17,22 @@ public class App
 {
 
     /**
-     * Return a list of JIRA tickets present in the provided gitCommits
-     */
+   * This method makes use of the regex matcher and pattern operators in order to 
+   * return a list of JIRA tickets present in the provided gitCommits. The compiled
+   * pattern is one that conforms to the standards outlined by JIRA's API documentation.
+   */
     public List<String> getJiraTickets(List<GitCommit> gitCommits)
     {
-      // TODO: Implement me
-      return null;
+      List<String> tickets = new ArrayList<String>();
+        int n = 0;
+        Pattern p = Pattern.compile("((?<!([A-Za-z]{1,10})-?)[A-Z]+-\\d+)");
+        Matcher m = p.matcher(gitCommits.get(n).getMessage());
+
+        while (m.find()) {
+            tickets.add(m.group());
+            n++;
+        }
+        return tickets;
     }
 
 }
